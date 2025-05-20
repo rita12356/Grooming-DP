84e954e31ae3dfbdf46159d4d5b0247226346518
rita <wadeew2016@yandex.ru>
2025-05-20 16:16:53 +0500

    перенос текста

--- pages/index.vue
+++ pages/index.vue
@@ -10,8 +10,8 @@
         <p class="uppercase  border-b-4 border-b-neutral-800 font-extrabold text-[#25231b] text-[22px]">
           Сделай питомца <span class="font-bold text-amber-500"><br>счастливым</span>
         </p>
-        <p class="font-mono animate-bounce text-[#25231b] text-[22px]">
-          Профессиональный груминг для домашних животных
+        <p class="font-mono animate-bounce mt-10 text-[#25231b] text-[22px]">
+          Профессиональный груминг <br>для домашних животных
         </p>
       </div>
 
