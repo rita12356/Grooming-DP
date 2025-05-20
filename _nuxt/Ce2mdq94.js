7b67357adfabe2d0644c151f1e4931f4dbd4b6bc
rita <wadeew2016@yandex.ru>
2025-05-20 16:25:20 +0500

    Updates

--- _nuxt/Ce2mdq94.js
+++
@@ -1,1 +0,0 @@
-import{u as t}from"./kyM8QN6H.js";import{D as e,n as o}from"./Bz8PgH43.js";const n=e(async(r,a)=>{if(!t().isAuthenticated)return o("/register")});export{n as default};
---
+++ _nuxt/Dp5nsROo.js
@@ -0,0 +1,1 @@
+import{u as t}from"./4zvJUC1J.js";import{D as e,n as o}from"./BbchC33O.js";const n=e(async(r,a)=>{if(t().isAuthenticated)return o("/record")});export{n as default};
